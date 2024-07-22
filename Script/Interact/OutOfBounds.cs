2dae4d246c083ae1361c40a1905775bf554eb9b3
Paul <arr.explore@gmail.com>
2024-07-22 01:59:35 -0400

    Updating the water shader

--- Script/Interact/OutOfBounds.cs
+++ Script/Interact/OutOfBounds.cs
@@ -4,11 +4,18 @@ namespace Game
 {
     public partial class OutOfBounds : Area3D
     {
+        [Export] public string SfxToPlay = "Splash";
+        
         private Node3D playerResetPosition;
+        private SFXPlayer3D sfxPlayer3D;
+        private CameraBridge cameraBridge;
 
         public override void _Ready()
         {
-            playerResetPosition = GetNode<Node3D>("Spawnpoint");
+            playerResetPosition = GetNode<Marker3D>("Spawnpoint");
+            sfxPlayer3D = GetNode<SFXPlayer3D>("/root/SFXPlayer3D");
+            cameraBridge = GetNode<CameraBridge>("/root/CameraBridge");
+
             BodyEntered += OnBodyEntered;
         }
 
@@ -16,6 +23,8 @@ namespace Game
         {
             if (body is CharacterBody3D characterBody)
             {
+                cameraBridge.Blink();
+                sfxPlayer3D.PlaySound(SfxToPlay, characterBody.GlobalPosition);
                 characterBody.GlobalTransform = new Transform3D(characterBody.GlobalTransform.Basis, playerResetPosition.GlobalTransform.Origin);
             }
         }
